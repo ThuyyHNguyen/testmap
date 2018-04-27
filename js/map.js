e9d4fc7d8d6667d8bd71a30a7f686ace7d972ee3
ThuyyHNguyen <emylymancy@gmail.com>
2018-04-27 21:11:25 +1200

    addfile

--- js/map.js
+++ js/map.js
@@ -1,3 +1,14 @@
+           function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
+
+
       function initMap() {
         var directionsService = new google.maps.DirectionsService;
         var directionsDisplay = new google.maps.DirectionsRenderer;
